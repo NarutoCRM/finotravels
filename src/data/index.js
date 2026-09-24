47aa3c7bd5719fbd4f4821c976c594f457a74122
Naruto Developer <developeratnaruto@gmail.com>
2026-09-24 22:15:43 +0530

    change phone number

--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,8 @@
 const appData = {
-appName: 'Fino Travels',
-phoneNumber: '8334110077',
-email:"contact@finotravels.com"
+  appName: "Fino Travels",
+  // phoneNumber: '8334110077',
+  phoneNumber: "8553854015",
+  email: "contact@finotravels.com",
+};
 
-}
-
-export { appData }
+export { appData };
